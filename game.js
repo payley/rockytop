92f695981eb9fc085b73d83ed40ae18cceec0754
Page Hayley <65619543+payley@users.noreply.github.com>
2020-12-11 16:48:45 -0600

    Update game.js

--- game.js
+++ game.js
@@ -3,9 +3,3 @@ function start(){
     var p = document.getElementById("players").value;
     var r = document.getElementById("rounds").value;
 }
-function climbon(){
-    var p1 = 0;
-    var p2 = 0;
-    var p3 = 0;
-    var p4 = 0;
-}
