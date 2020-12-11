a1c2aab410780aa4da5286baedc4f8cf645a4420
Page Hayley <65619543+payley@users.noreply.github.com>
2020-12-11 17:41:26 -0600

    Update game.js

--- game.js
+++ game.js
@@ -4,8 +4,4 @@ function start(){
     var r = document.getElementById("rounds").value;
     document.getElementById("value1").innerHTML = p; 
     document.getElementById("value2").innerHTML = r;
-    
-    for (var i = 1; i == p; i++){
-    var id = "p" + i;
-    document.getElementById(id).style.display = 'block';
-}
+    document.getElementById("p1").style.display = 'block';
