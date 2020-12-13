5a99f35a8c134e554ff8cd497a4977226c69c013
Page Hayley <65619543+payley@users.noreply.github.com>
2020-12-13 05:25:00 -0600

    Update game.js

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ document.getElementById('date').innerHTML = new Date().toDateString();
 function start(){
     var p = document.getElementById("players").value;
     var r = document.getElementById("rounds").value;
-    document.getElementById("value1").innerHTML = p;
+    document.getElementById("value1").innerHTML = r;
     for (var i = 0; i < p; i++){
        var idx = document.getElementById("P" + i)
        idx.innerHTML = "Player_" + i;
