e15475717a8f4c84b80d0ca003a9caea4950e873
Page Hayley <65619543+payley@users.noreply.github.com>
2020-12-13 21:32:16 -0600

    Update game.js

--- game.js
+++ game.js
@@ -1,16 +1,31 @@
-document.getElementById('date').innerHTML = new Date().toDateString();
-function start(){
-    var p = document.getElementById("players").value;
-    var r = document.getElementById("rounds").value;
-    document.getElementById("value1").innerHTML = r;
+document.getElementById("value1").innerHTML = "&nbsp" + "1";
+var p = 0;
+var r = 0;
+var turn = 1;
+
+function start() {
+    p = document.getElementById("players").value;
+    r = document.getElementById("rounds").value;
+    var PL = new Array(p);
+    for (var i = 0; i < p; i++) {
+        var num = i + 1;
+        PL[i] = "Player" + '&nbsp' + num;
+    }
+    for (var i = 0; i < PL.length; i++) {
+        var num = i + 1;
+        var idx = document.getElementById("P" + num);
+        idx.innerHTML = "Player" + "&nbsp" + num;
+        var idx = document.getElementById("R" + num);
+        idx.innerHTML = "Round" + "&nbsp" + 0 + "/" + r;
+    }
+    var turn = 1;
+    document.getElementById("value1").innerHTML = "&nbsp" + turn;
 }
-function run(){
-    var num = p + 1;
-    var i = 0;
-    for (var i = 1; i < p; i++){
-        var idx = document.getElementById("P" + i);
-        idx.innerHTML = "Player_" + i;
-        document.getElementById("value2").innerHTML = i;
-}
-        document.getElementById("value2").innerHTML = i;
+
+function next() {
+    turn = turn + 1;
+    if (turn > p) {
+        turn = 1;
+    }
+    document.getElementById("value1").innerHTML = "&nbsp" + turn;
 }
