6f0ec0fe6e7fb204725e8ba078762a9605814d79
Page Hayley <65619543+payley@users.noreply.github.com>
2020-12-17 14:32:51 -0600

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
-document.getElementsByClassName("value1").innerHTML= "&nbsp" + 4;
 var init = document.getElementsByClassName("load");
 for (var i = 0; i < init.length; i++) {
-    init[i].style.display = "none";
+  init[i].style.display = "none";
 }
+
+document.getElementById("value1").innerHTML =  "Player" + "&nbsp" + 1 + "'s" + "&nbsp" + "turn";
+
 
 var p = 0;
 var r = 0;
@@ -15,72 +17,72 @@ var val = 0;
 var sc = 0;
 
 function start() {
-    p = document.getElementById("players").value;
-    r = document.getElementById("rounds").value;
-    var PL = new Array(p);
-    rnd = new Array(p);
-    for (var i = 0; i < p; i++) {
-        var num = i + 1;
-        PL[i] = "Player" + '&nbsp' + num;
-        rnd[i] = 1;
-    }
-    for (var i = 0; i < PL.length; i++) {
-        var num = i + 1;
-        var idp = document.getElementById("P" + num);
-        idp.innerHTML = "Player" + "&nbsp" + num;
-        var idr = document.getElementById("S" + num);
-        idr.innerHTML = 0;
-        var idr = document.getElementById("R" + num);
-        idr.innerHTML = "Round" + "&nbsp" + rnd[i] + "/" + r;
-        var idi = document.getElementById("I" + num);
-        idi.style.display = "Block";
-    }
+  p = document.getElementById("players").value;
+  r = document.getElementById("rounds").value;
+  var PL = new Array(p);
+  rnd = new Array(p);
+  for (var i = 0; i < p; i++) {
+    var num = i + 1;
+    PL[i] = "Player" + '&nbsp' + num;
+    rnd[i] = 1;
+  }
+  for (var i = 0; i < PL.length; i++) {
+    var num = i + 1;
+    var idp = document.getElementById("P" + num);
+    idp.innerHTML = "Player" + "&nbsp" + num;
+    var idr = document.getElementById("S" + num);
+    idr.innerHTML = 0;
+    var idr = document.getElementById("R" + num);
+    idr.innerHTML = "Round" + "&nbsp" + rnd[i] + "/" + r;
+    var idi = document.getElementById("I" + num);
+    idi.style.display = "Block";
+  }
 }
 
 function next() {
-    var val = parseFloat(document.getElementById("input" + turn).value);
-    var sc = parseFloat(document.getElementById("S" + turn).innerHTML);
-    var abs = turn - 1;
-    var ref = (rnd[abs]) - 1;
-    if (val > pk[ref]) {
-        score = sc + (val - pk[ref]);
-        document.getElementById("S" + turn).innerHTML = score; 
-        if (score == pk[ref]) {
-            rnd[abs] = rnd[abs] + 1;
-            document.getElementById("S" + turn).innerHTML = 0;
-            document.getElementById("R" + turn).innerHTML = "Round" + "&nbsp" + rnd[abs] + "/" + r;
-        } else if (score > pk[ref]) {
-          document.getElementById("S" + turn).innerHTML = 0;
-        }
-    } else if (val < pk[ref]) {
-        score = sc + val;
-        document.getElementById("S" + turn).innerHTML = score;
-        if (score == pk[ref]) {
-            rnd[abs] = rnd[abs] + 1;
-            document.getElementById("S" + turn).innerHTML = 0;
-            document.getElementById("R" + turn).innerHTML = "Round" + "&nbsp" + rnd[abs] + "/" + r;
-        } else if (score > pk[ref]) {
-          document.getElementById("S" + turn).innerHTML = 0;
-        }
-    } else if (val = pk[ref]) {
-        score = 0;
-        document.getElementById("S" + turn).innerHTML = score;
-        rnd[abs] = rnd[abs] + 1;
-        document.getElementById("R" + turn).innerHTML = "Round" + "&nbsp" + rnd[abs] + "/" + r;
+  var val = parseFloat(document.getElementById("input" + turn).value);
+  var sc = parseFloat(document.getElementById("S" + turn).innerHTML);
+  var abs = turn - 1;
+  var ref = (rnd[abs]) - 1;
+  if (val > pk[ref]) {
+    score = sc + (val - pk[ref]);
+    document.getElementById("S" + turn).innerHTML = score;
+    if (score == pk[ref]) {
+      rnd[abs] = rnd[abs] + 1;
+      document.getElementById("S" + turn).innerHTML = 0;
+      document.getElementById("R" + turn).innerHTML = "Round" + "&nbsp" + rnd[abs] + "/" + r;
+    } else if (score > pk[ref]) {
+      document.getElementById("S" + turn).innerHTML = 0;
     }
-    document.getElementById("input" + turn).value = "";
-    turn = turn + 1;
-    prev = turn;
-    if (turn > p) {
-        turn = 1;
+  } else if (val < pk[ref]) {
+    score = sc + val;
+    document.getElementById("S" + turn).innerHTML = score;
+    if (score == pk[ref]) {
+      rnd[abs] = rnd[abs] + 1;
+      document.getElementById("S" + turn).innerHTML = 0;
+      document.getElementById("R" + turn).innerHTML = "Round" + "&nbsp" + rnd[abs] + "/" + r;
+    } else if (score > pk[ref]) {
+      document.getElementById("S" + turn).innerHTML = 0;
     }
-  if(rnd[abs] > r){
-      document.getElementById("value2").innerHTML = "Player" + "&nbsp" + (prev-1) + "&nbsp" + "wins!";
-      document.getElementById("value1").innerHTML = "";
-      document.getElementById("msg").innerHTML = "";
-      document.getElementById("R" + (prev - 1)).innerHTML = "Round" + "&nbsp" + r + "/" + r;
-      document.getElementById("button2").disabled = true;
+  } else if (val == pk[ref]) {
+    score = 0;
+    document.getElementById("S" + turn).innerHTML = score;
+    rnd[abs] = rnd[abs] + 1;
+    document.getElementById("R" + turn).innerHTML = "Round" + "&nbsp" + rnd[abs] + "/" + r;
+  }
+  document.getElementById("input" + turn).value = "";
+  turn = turn + 1;
+  prev = turn;
+  if (turn > p) {
+    turn = 1;
+  }
+  if (rnd[abs] > r) {
+    document.getElementById("value2").innerHTML = "Player" + "&nbsp" + (prev - 1) + "&nbsp" + "wins!";
+    document.getElementById("button2").disabled = true;
+    document.getElementById("R" + (prev-1)).innerHTML = "Round" + "&nbsp" + r + "/" + r;
+    document.getElementById("value1").innerHTML = "";
+
   } else {
-      document.getElementById("value1").innerHTML = "&nbsp" + turn;
+    document.getElementById("value1").innerHTML = "Player" + "&nbsp" + turn + "'s" + "&nbsp" + "turn";
   }
 }
